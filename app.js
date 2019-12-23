12f430a83a7a7cea9f6fe61601a07306cc6c0671
vattcarter7 <vattcarter@gmail.com>
2019-12-23 18:07:08 +0700

    implement pug template route, created tourOverview page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -11,10 +12,18 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // GLOBAL MIDDLEWARES
+
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // SET SECURITY HTTP headers
 app.use(helmet());
 
@@ -57,10 +66,6 @@ app.use(
   })
 );
 
-// SERVING STATIC FILES
-// find the static html files or other files in the public directory
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -70,6 +75,8 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
